73654b2401fcbae4d10e188342a8eba91b031d45
vsicilia <vsicilia@kent.edu>
2017-04-02 16:46:27 -0400

    JS examples provided in intro to web dev

---
+++ JScourseExamples/CountingCharacters/countingCharacters.js
@@ -0,0 +1,55 @@
+/* function countingCharacters(stringToCount){
+   
+    console.log (stringToCount + " has " + stringToCount.length + " characters.");
+}
+
+function countingCharacters2(stringToCount, characterToFind){
+    var characterCount = 0;
+    for (var characterPosition = 0; characterPosition < stringToCount.length; characterPosition++){
+        if (stringToCount[characterPosition] == characterToFind){
+            characterCount++;
+        }
+    }
+    console.log("String to search in: " + stringToCount);
+    console.log("Character to find: " + characterToFind);
+    console.log("Number of times the character appears: " + 
+                  characterCount);
+}
+
+*/
+
+
+
+
+function countingCharacters(str, search){
+    
+    var count = 0;
+    var numChars = search.length;
+    
+    var lastIndex = str.length - numChars;
+    
+    for (var index = 0; index <= lastIndex; index++){
+        
+       var current = str.substring(index, index + numChars);
+       
+        if (current === search){            
+            count++;
+        }
+    }
+    
+    var numChars = search.length; 
+    var lastIndex = str.length - numChars;
+        
+    
+    console.log("String to search in: " + str);
+    console.log("Character to find: " + search);
+    console.log("Number of times the character appears: " + count);
+    
+    return count;
+}
+
+
+
+
+
+countingCharacters3("xxaxxxbxx","xx");
